bddb6530b7bea0ebc344b54049229a719ae90015
Qreamville <Oyeleyebalikis@yahoo.com>
2022-07-22 00:17:01 -1200

    Update index.test.ts

--- 7 kyu/is-it-negatve-zero/index.test.ts
+++ 7 kyu/is-it-negatve-zero/index.test.ts
@@ -4,15 +4,19 @@ describe("Testing solutions", () => {
   test("Negative zeros equates to true", () => {
     expect(negativeZero(-0)).toBeTruthy();
   });
+
   test("Negative zeros equates to true", () => {
     expect(negativeZero(0)).toBeFalsy();
   });
+
   test("Negative zeros equates to true", () => {
     expect(negativeZero(-9)).toBeFalsy();
   });
+
   test("Negative zeros equates to true", () => {
     expect(negativeZero(-8)).toBeFalsy();
   });
+
   test("Negative zeros equates to true", () => {
     expect(negativeZero(10)).toBeFalsy();
   });
