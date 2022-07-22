5eda6dcc29d1f1295987ff5fc6975d744ad02b54
Qreamville <Oyeleyebalikis@yahoo.com>
2022-07-22 00:07:49 -1200

    Add test files

---
+++ 7 kyu/is-it-negatve-zero/index.test.ts
@@ -0,0 +1,19 @@
+const negativeZero = require("./index.js");
+
+describe("Testing solutions", () => {
+  test("Negative zeros equates to true", () => {
+    expect(negativeZero(-0)).toBeTruthy();
+  });
+  test("Negative zeros equates to true", () => {
+    expect(negativeZero(0)).toBeFalsy();
+  });
+  test("Negative zeros equates to true", () => {
+    expect(negativeZero(-9)).toBeFalsy();
+  });
+  test("Negative zeros equates to true", () => {
+    expect(negativeZero(-8)).toBeFalsy();
+  });
+  test("Negative zeros equates to true", () => {
+    expect(negativeZero(10)).toBeFalsy();
+  });
+});
